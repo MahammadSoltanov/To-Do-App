42d504aa49cff5735e369927f5f5a80216cbe26f
MaqaSoltanov <maqa.sltnv@gmail.com>
2022-11-03 13:30:03 +0400

    Added sort button and defined its behavior on mouseover and mouseout. Created template for task, input and delete button.

---
+++ Core/logic.js
@@ -0,0 +1,11 @@
+const sortImage = document.querySelector('.sort-button');
+sortImage.addEventListener('mouseover', (event) =>{sortImage.src = '/Images/sortAscendingBlack.svg';});
+sortImage.addEventListener('mouseout', (event) =>{sortImage.src = '/Images/sortAscendingGray.svg';});
+
+const xImage = document.querySelector('.x-button');
+xImage.addEventListener('mouseover', (event) =>{xImage.src = '/Images/xButtonBlack.svg';});
+xImage.addEventListener('mouseout', (event) =>{xImage.src = '/Images/xButtonGray.svg';});
+
+
+
+
